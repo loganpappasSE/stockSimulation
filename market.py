32e15bf4f1d00dff897beedb9bc508edf4bdf20a
loganp2524 <pappaslogan@gmail.com>
2026-03-04 13:57:15 -0600

    working rn on changing percentage in market, then player

--- market.py
+++ market.py
@@ -3,33 +3,35 @@ import random
 class Market:
     def __init__ (self):
         self.stocks - {
-            "AAPL": {"price": 180, "risk": 0.2},
-            "MSFT": {"price": 320, "risk": 0.2},
-            "GOOG": {"price": 140, "risk": 0.3},
-            "AMZN": {"price": 130, "risk": 0.4},
-            "META": {"price": 310, "risk": 0.5},
+            "AAPL": {"price": 180, "risk": 0.2,"previous":0},
+            "MSFT": {"price": 320, "risk": 0.2,"previous":0},
+            "GOOG": {"price": 140, "risk": 0.3,"previous":0},
+            "AMZN": {"price": 130, "risk": 0.4,"previous":0},
+            "META": {"price": 310, "risk": 0.5,"previous":0},
 
-            "TSLA": {"price": 240, "risk": 0.8},
-         "NVDA": {"price": 450, "risk": 0.7},
-            "AMD": {"price": 120, "risk": 0.6},
-            "INTC": {"price": 40, "risk": 0.4},
-            "ORCL": {"price": 110, "risk": 0.3},
+            "TSLA": {"price": 240, "risk": 0.8,"previous":0},
+         "NVDA": {"price": 450, "risk": 0.7,"previous":0},
+            "AMD": {"price": 120, "risk": 0.6,"previous":0},
+            "INTC": {"price": 40, "risk": 0.4,"previous":0},
+            "ORCL": {"price": 110, "risk": 0.3,"previous":0},
 
-            "NFLX": {"price": 430, "risk": 0.6},
-            "DIS": {"price": 95, "risk": 0.4},
-            "UBER": {"price": 65, "risk": 0.7},
-            "SHOP": {"price": 75, "risk": 0.8},
-            "SQ": {"price": 70, "risk": 0.7},
+            "NFLX": {"price": 430, "risk": 0.6,"previous":0},
+            "DIS": {"price": 95, "risk": 0.4,"previous":0},
+            "UBER": {"price": 65, "risk": 0.7,"previous":0},
+            "SHOP": {"price": 75, "risk": 0.8,"previous":0},
+            "SQ": {"price": 70, "risk": 0.7,"previous":0},
 
-            "COIN": {"price": 95, "risk": 0.9},
-            "SNAP": {"price": 20, "risk": 0.8},
-            "ROKU": {"price": 60, "risk": 0.7},
-            "PLTR": {"price": 25, "risk": 0.6},
-            "RIVN": {"price": 18, "risk": 0.9}
+            "COIN": {"price": 95, "risk": 0.9,"previous":0},
+            "SNAP": {"price": 20, "risk": 0.8,"previous":0},
+            "ROKU": {"price": 60, "risk": 0.7,"previous":0},
+            "PLTR": {"price": 25, "risk": 0.6,"previous":0},
+            "RIVN": {"price": 18, "risk": 0.9,"previous":0}
 
         }
-    
-    def change_price():
+    def set_previous(self):
+       for stock in self.stocks.values:
+          stock["previous"]=stock["price"]
+    def change_price(self):
         factor = random.uniform(.1,.9)
         lucky_day=random.randint(1,10)
         for stock in self.stocks.values:
@@ -45,4 +47,9 @@ class Market:
            symbol = random.choice(list(self.stocks.keys()))
            self.stocks[symbol]["price"] *=8
     
+    def new_day(self):
+       self.set_previous()
+       self.change_price()
+    def percentchange(self):
+       
       
--- player.py
+++ player.py
@@ -12,3 +12,21 @@ class Player:
             portfolio +=1
         else:
             portfolio[stock]=1
+    def delete_stock(self, stock,player):
+        portfolio = self.player[player]["portfolio"]
+        if stock in portfolio:
+            portfolio -=1
+    def buy_stock(self,stock,player,market):
+        price = market.stocks[stock]["price"]
+        self.player[player]["money"] -= price
+        self.add_stock(stock,player)
+    def sell_stock(self,stock,player,market):
+        price = market.stocks[stock]["price"]
+        self.delete_stock()
+        self.player[player]["money"] += price
+    def display(self,player,market):
+        print(f"In {player}'s portfolio, you have ")
+        for item,amount in self.player["portfolio"].items:
+            print(item, ":",amount,"shares  the price today is", 
+                  market.stocks[item]["price"], " and has gone up " )
+
